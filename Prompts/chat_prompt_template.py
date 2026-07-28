d8f8fa709364e77324a248b75a56f3561643fc1b
yourshaswat <shaswatdhs@gmail.com>
2026-07-28 23:26:56 +0530

    Chat prompt template for dynamic (list of messages)

---
+++ Prompts/chat_prompt_template.py
@@ -0,0 +1,9 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template=ChatPromptTemplate([
+    ('system', "You are a helpful {domain} expert"),
+    ('human', 'Explain in simple terms, what is {topic}')
+])
+
+prompt= chat_template.invoke({'domain': 'Cricket', 'topic': 'dusra'})
+print(prompt)
