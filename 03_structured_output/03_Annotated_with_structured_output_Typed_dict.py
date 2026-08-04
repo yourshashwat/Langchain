88f066debdc161235fc4852926c655fe2010cc33
yourshaswat <shaswatdhs@gmail.com>
2026-08-04 14:02:56 +0530

    used Annotated, optional and Literal

---
+++ 03_structured_output/03_Annotated_with_structured_output_Typed_dict.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from typing import Annotated, Optional, Literal, TypedDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model= ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+class Review(TypedDict):
+    key_themes: Annotated[list[str], "Write all the key theemes in the form of list"]
+    summary: Annotated[str, "Write a brief summary from the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+    name: Annotated[Optional[str], "Extract ONLY the reviewer's personal name. Do NOT return the product name, company name, or brand. If no reviewer name is mentioned, return null."]
+
+structured_model= model.with_structured_output(Review)
+
+result= structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh
+""")
+
+print(result)
